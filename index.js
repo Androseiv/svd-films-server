2b850e8066f7bd7d07114cae6ea583cdcbd7f28f
Androseiv <kakatinushka@gmail.com>
2022-01-20 13:05:09 +0200

    port

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
+  origin: process.env.CLIENT_URL,
+  secure: true
 }));
 app.use('/api', router)
 app.use(errorMiddleware)
