0399ed7007ce208f2f4509ba21a54917c695c7b4
Androseiv <kakatinushka@gmail.com>
2022-01-23 18:55:47 +0200

    test

--- router/index.js
+++ router/index.js
@@ -23,19 +23,19 @@ router.post('/logout', UserController.logout);
 router.get('/activate/:link', UserController.activate);
 router.get('/refresh', UserController.refresh);
 
-router.get('/:userId/films/favourite/', FilmController.getFavouriteFilms);
+router.get('/:user_id/films/favourite/', FilmController.getFavouriteFilms);
 router.post('/films/favourite/add', authMiddleware, FilmController.addFavouriteFilm);
 router.post('/films/favourite/remove', authMiddleware, FilmController.removeFavouriteFilm);
 
-router.get('/:userId/films/later', FilmController.getLaterFilms);
+router.get('/:user_id/films/later', FilmController.getLaterFilms);
 router.post('/films/later/add', authMiddleware, FilmController.addLaterFilm);
 router.post('/films/later/remove', authMiddleware, FilmController.removeLaterFilm);
 
-router.get('/:userId/films/rated', FilmController.getRatedFilms);
+router.get('/:user_id/films/rated', FilmController.getRatedFilms);
 router.post('/films/rated/add', authMiddleware, FilmController.addRatedFilm);
 router.post('/films/rated/remove', authMiddleware, FilmController.removeRatedFilm);
 
-router.get('/info/film/:filmId/:userId', authMiddleware, FilmController.userFilm);
-router.get('/info/user/:userId', UserController.getUserInfo);
+router.get('/info/film/:film_id/:user_id', authMiddleware, FilmController.userFilm);
+router.get('/info/user/:user_id', UserController.getUserInfo);
 
 module.exports = router;
